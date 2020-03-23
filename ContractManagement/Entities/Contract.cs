5aff604c801f44a30836be54c4d7173b09af6b23
Jonathan <jonathanpalma89@gmail.com>
2020-03-23 20:03:39 +0000

    chore: add Entities (Contract and Installment)

---
+++ ContractManagement/Entities/Contract.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContractManagement.Entities
+{
+    class Contract
+    {
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+        public double ContractValue { get; set; }
+
+        public Contract(int number, DateTime date, double contractValue)
+        {
+            Number = number;
+            Date = date;
+            ContractValue = contractValue;
+        }
+    }
+}
---
+++ ContractManagement/Print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ContractManagement.Entities;
+
+namespace ContractManagement
+{
+    class Print
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the contract details");
+            Console.Write("Contract ID: ");
+            int id = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/mm/yyyy): ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            Console.Write("Contract value: ");
+            double value = double.Parse(Console.ReadLine());
+
+            Contract contract = new Contract(id, date, value);
+        }
+    }
+}
