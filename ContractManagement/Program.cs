957def49eb0a74681227d76f9dd38fb4b6757132
Jonathan <jonathanpalma89@gmail.com>
2020-03-23 19:15:51 +0000

    chore: add new project

---
+++ ContractManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ContractManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
